79ab9bafa0d142ff9db6b0e391983f28cb4f3d2b
Sean D. Cline <sean@sdcline.com>
2026-05-13 08:55:38 -0400

    Clean up URL redirect logic to remove  a level of indirection.

---
+++ redirects.js
@@ -0,0 +1,6 @@
+// Case-insensitive redirects handled by 404.html.
+// Keys are lowercase slugs. Values are the destination URL.
+var url_redirects = {
+	'rabidkeyboard': 'https://github.com/SeanCline/RabidKeyboard',
+	'atari8bitcart':  'https://github.com/SeanCline/atari8bitcart'
+};
